ed4c60d4ea116169f3fe0ba956e3d00a4dc400a8
Joshua Duncan <joshuaduncan@Joshuas-MacBook-Pro.local>
2020-10-18 21:39:32 -0600

    setting up apis

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -1,12 +1,58 @@
 var currentDate = moment().format('L');
-var currentCity = ""
+var currentCity = "";
+var currentLon = "";
+var currentLat = "";
 
 function firstRender() {
     $("#curNameDate").text(currentCity + ": " + currentDate)
     console.log(currentDate)
 }
 
-$("#findCity").click(function(){
+$("#findCity").click(function(event){
+    event.preventDefault();
     currentCity = $("#cityName").val()
-    firstRender()
-})
+    firstRender();
+    getWeather();
+    
+});
+
+function getWeather() {
+    var apiKey = "b83223f78956aa8a1f4ff4a30fa9435f"
+    var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + currentCity + "&appid=" + apiKey
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response){
+        console.log(response);
+        currentLon = response.coord.lon;
+        currentLat = response.coord.lat;
+        var temp = (response.main.temp - 273.15) * 1.80 + 32;
+        var humidity = response.main.humidity;
+        var windSpeed = response.wind.speed;
+        
+        console.log("Temperature: " + temp + " f");
+        console.log("Humidity: " + humidity + "%");
+        console.log("Wind Speed: " + windSpeed + " MPH");
+        console.log("Longitude: " + currentLon)
+        console.log("Latitude: " + currentLat)
+        getUV();
+    })
+    
+};
+
+function getUV(){
+    var apiKey = "b83223f78956aa8a1f4ff4a30fa9435f"
+    var queryURL = "http://api.openweathermap.org/data/2.5/uvi?lat=" + currentLat + "&lon=" + currentLon + "&appid=" + apiKey
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response){
+        console.log(response);
+        var uvIndex = response.value
+        console.log("UV Index: " + uvIndex)
+        
+    })
+};
+
