961cdfb871aeeea302050547f0d3575a45f6ffd2
Joshua Duncan <joshuaduncan@Joshuas-MacBook-Pro.local>
2020-10-22 16:00:41 -0600

    restored previous commit

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -14,8 +14,11 @@ function firstRender() {
         displayCity = "Salt Lake City"
         cities.push(displayCity)
         localStorage.setItem("cities", JSON.stringify(cities))
+        //$("#curNameDate").text(displayCity + " : " + currentDate)
     } else {
         displayCity = lastCityParse[lastCityParse.length - 1]
+        //$("#curNameDate").text(displayCity + ": " + currentDate)
+        console.log(displayCity) 
     }
     getLocation()
     renderButtons()
@@ -31,7 +34,8 @@ function getLocation() {
     }).then(function (response) {
         currentLon = response.coord.lon;
         currentLat = response.coord.lat;
-
+        console.log("Latitude: " + currentLat)
+        console.log("Longitude: " + currentLon)
         getWeather()
     });
 }
@@ -44,6 +48,7 @@ $("#findCity").click(function (event) {
     currentCity = $("#cityName").val()
     cities.push(currentCity)
     localStorage.setItem("cities", JSON.stringify(cities))
+
     cityData = localStorage.getItem("cities");
     lastCityParse = JSON.parse(cityData);
     firstRender();
@@ -68,16 +73,18 @@ function getWeather() {
         method: "GET"
     }).then(function (response) {
         console.log(response)
-        var uv = response.current.uvi
-        var currIcon = $("<img>").addClass("current imageIcon").attr("src", "http://openweathermap.org/img/wn/" + response.current.weather[0].icon + "@2x.png")
-        $("#curNameDate").text(displayCity + " : " + currentDate);
-        $("#curNameDate").append(currIcon);
+        uv = response.current.uvi
+        $("#curNameDate").text(displayCity + " : " + currentDate)
         var temp = $("<p>").addClass("current").text("Temperature: " + ((response.current.temp - 273.15) * 1.80 + 32).toFixed(2) + " " + String.fromCharCode(8457));
         var humidity = $("<p>").addClass("current").text("Humidity: " + response.current.humidity + " %");
         var windSpeed = $("<p>").addClass("current").text("Wind Speed: " + response.current.wind_speed + " MPH");
         var uvIndex = $("<p>").addClass("current").text("UV Index: ").append("<span id=uvColor class'>" + uv + '</span>');
 
         $("#curWeather").append(temp, humidity, windSpeed, uvIndex);
+        console.log(uv)
+        console.log("Icon: " + "http://openweathermap.org/img/wn/" + response.daily[1].weather[0].icon + "@2x.png")
+        console.log("Temp: " + ((response.daily[1].temp.day - 273.15) * 1.80 + 32).toFixed(2) + " f")
+        console.log("Humidity: " + response.daily[1].humidity + " %")
 
         for (var i = 1; i <= 5; i++) {
             var epochDate = response.daily[i].dt;
